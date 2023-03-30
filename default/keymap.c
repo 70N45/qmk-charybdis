7ccdcdcf4745cc140b9f2ce441dd9bba6090db71
Jonas Bernsdorff <jbernsdorff@palantir.com>
2023-03-30 13:35:44 +0200

    update

--- default/keymap.c
+++ default/keymap.c
@@ -13,6 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
  */
 
 #include QMK_KEYBOARD_H
@@ -26,17 +27,15 @@ enum charybdis_keymap_layers {
 #define LOWER MO(LAYER_LOWER)
 #define RAISE MO(LAYER_RAISE)
 
-#define CTL_BSP CTL_T(KC_BSPC)
-#define SFT_SPC SFT_T(KC_SPC)
-#define GUI_ENT GUI_T(KC_ENT)
+#define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_RAISE
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
-          KC_Q,          KC_W,          KC_F,                    KC_P,          KC_B,                          KC_J,     KC_L,          KC_U,          KC_Y,          MT(LSFT(KC_SCLN), KC_SCLN),
+          KC_Q,          KC_W,          KC_F,                    KC_P,          KC_B,                          KC_J,     KC_L,          KC_U,          KC_Y,          KC_SCLN,
           LSFT_T(KC_A),  LCTL_T(KC_R),  LALT_T(KC_S),            LGUI_T(KC_T),  KC_G,                          KC_M,     RGUI_T(KC_N),  RALT_T(KC_E),  RCTL_T(KC_I),  RSFT_T(KC_O),
-          KC_Z,          KC_X,          MT(LGUI(KC_C), KC_C),    KC_D,          MT(LGUI(KC_V), KC_V),          KC_K,     KC_H,          KC_COMM,       KC_DOT,        MT(LSFT(KC_SLSH), KC_SLSH),
-                                        KC_BSPC,                 RAISE,         LGUI_T(KC_ESC),                LOWER,    MEH_T(KC_SPC)
+          KC_Z,          KC_X,          KC_C,                    KC_D,          KC_V,                          KC_K,     KC_H,          KC_COMM,       KC_DOT,        KC_SLSH,
+                                        KC_BSPC,                 RAISE,         LGUI_T(KC_ESC),                LOWER, MEH_T(KC_SPC)
   ),
 
   [LAYER_LOWER] = LAYOUT(
@@ -47,10 +46,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [LAYER_RAISE] = LAYOUT(
-          RSG(KC_4),     DRGSCRL,       KC_BTN1,       KC_BTN2,       KC_NO,              KC_HOME,  KC_PGDN,       KC_PGUP,       KC_END,        DPI_MOD, 
-          KC_LSFT,       KC_LCTL,       KC_LALT,       KC_LGUI,       KC_NO,              KC_LEFT,  KC_DOWN,       KC_UP,         KC_RGHT,       KC_DEL, 
-          RGB_MOD,       RGB_RMOD,      RGB_VAI,       RGB_VAD,       RGB_SPI,            SNP_TOG,  KC_NO,         KC_ENT,        KC_TAB,        DPI_RMOD, 
-                                        KC_TRNS,       KC_TRNS,       KC_TRNS,            KC_TRNS,  KC_TRNS
+          RGB_HUI,     DRGSCRL,       KC_BTN1,       KC_BTN2,       RGB_TOG,                KC_HOME,  KC_PGDN,       KC_PGUP,       KC_END,        DPI_MOD, 
+          KC_LSFT,       KC_LCTL,       KC_LALT,       KC_LGUI,       KC_DEL,               KC_LEFT,  KC_DOWN,       KC_UP,         KC_RGHT,       KC_BSPC, 
+          RGB_MOD,       RGB_SPD,       RGB_SPI,       RGB_VAI,       RGB_VAD,              S_D_MOD,  S_D_RMOD,         KC_ENT,        KC_TAB,   DPI_RMOD, 
+                                        KC_TRNS,       KC_TRNS,       KC_TRNS,              KC_TRNS,  KC_TRNS
   ),
 };
+
+
+#if defined(POINTING_DEVICE_ENABLE) && defined(CHARYBDIS_AUTO_SNIPING_ON_LAYER)
+layer_state_t layer_state_set_kb(layer_state_t state) {
+    state = layer_state_set_user(state);
+    charybdis_set_pointer_sniping_enabled(layer_state_cmp(state, CHARYBDIS_AUTO_SNIPING_ON_LAYER));
+    return state;
+}
+#endif // POINTING_DEVICE_ENABLE && CHARYBDIS_AUTO_SNIPING_ON_LAYER
 // clang-format on
